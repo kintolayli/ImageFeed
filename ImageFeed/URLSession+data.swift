bf47e28e222a8218484990c556fc03423f594e78
Ilya Lotnik <i@lotnik.ru>
2024-06-14 01:12:16 +0300

    Подготовить проект к ревью - 2

--- ImageFeed/URLSession+data.swift
+++ ImageFeed/URLSession+data.swift
@@ -29,11 +29,14 @@ extension URLSession {
                 if 200 ..< 300 ~= statusCode {
                     fulfillCompletionOnTheMainThread(.success(data))
                 } else {
+                    print("NetworkError: HTTP status code: \(statusCode)")
                     fulfillCompletionOnTheMainThread(.failure(NetworkError.httpStatusCode(statusCode)))
                 }
             } else if let error = error {
+                print("NetworkError: \(error.localizedDescription)")
                 fulfillCompletionOnTheMainThread(.failure(NetworkError.urlRequestError(error)))
             } else {
+                print("NetworkError: URLSession error")
                 fulfillCompletionOnTheMainThread(.failure(NetworkError.urlSessionError))
             }
         })
